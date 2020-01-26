6533f01d4c0cffba8e7a52733a9c65ad9389ea28
Engr. Muhammad Anis Ur rehman <33159177+Aneesawan34@users.noreply.github.com>
2020-01-26 15:34:17 +0500

    promise in function

--- index.js
+++ index.js
@@ -1,8 +1,42 @@
+// --------- basics ------------
+
 let prom = new Promise(function(resolve, reject) {
   let task = 1 + 1;
   task == 2 ? resolve("Success") : reject("Failed");
 });
 
+// if condition is true then run resolve function otherwise run reject function
+// promise resolve then we call by .then if promise reject then call by .catch
+
 prom
   .then(msg => console.log("It is in the " + msg))
   .catch(msg => console.log("It is in the " + msg));
+
+// // -------- medium -------------
+
+// const userLeft = true;
+// const userWatchingCatchMeme = false;
+
+// function watchTutorialPromise() {
+//   return new Promise(function(resolve, reject) {
+//     userLeft
+//       ? reject({
+//           name: "User Left",
+//           message: ":("
+//         })
+//       : userWatchingCatchMeme
+//       ? reject({
+//           name: "User Watching Cat meme",
+//           message: "WebDevSimplified < Cat"
+//         })
+//       : resolve("Thunmbs Up and Subscribe");
+//   });
+// }
+
+// watchTutorialPromise()
+//   .then(msg => console.log("Success: ", msg))
+//   .catch(err =>
+//     console.log(
+//       "User Name: " + err.name + "\n" + "User Message: " + err.message
+//     )
+//   );
